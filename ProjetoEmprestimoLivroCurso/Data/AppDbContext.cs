8253bcf114bb3c7d1f818567b6a393b756452d27
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-07-16 19:03:59 -0300

    Add project files.

---
+++ ProjetoEmprestimoLivroCurso/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEmprestimoLivroCurso.Models;
+
+namespace ProjetoEmprestimoLivroCurso.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<LivroModel> Livros { get; set; }
+    }
+}
---
+++ ProjetoEmprestimoLivroCurso/Models/LivroModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoEmprestimoLivroCurso.Models
+{
+    public class LivroModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; } = string.Empty;
+        [Required]
+        public string Descricao { get; set; } = string.Empty;
+        [Required]
+        public string Capa { get; set; } = string.Empty;
+        [Required]
+        public string ISBN { get; set; } = string.Empty;
+
+        public string Autor { get; set; } = string.Empty;
+        [Required]
+        public string Genero { get; set; } = string.Empty;
+        [Required]
+        public int AnoPublicacao { get; set; }
+        [Required]
+        public int QuatidadeEmEstoque { get; set; }
+        public DateTime DataDeCadastro { get; set; } = DateTime.Now;
+        public DateTime DataDeAteracao { get; set; } = DateTime.Now;
+    }
+}
